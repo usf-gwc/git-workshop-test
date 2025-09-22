bc6a1b8d2319aa0eb4aadfec5d26e9a58830f7cc
usf-gwc <gwcusfloop@gmail.com>
2025-09-22 16:04:44 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Function prototypes
+int findSum(int x, int y);
+void printSum(int sum);
+
+int main(void) {
+    // Call the functions
+    int num1 = 10;
+    int num2 = 20;
+    int sum1 = findSum(num1, num2);
+    
+
+    return 0;
+}
+
+//function definitions
+int findSum(int x, int y){
+
+}
+
+void printSum(int sum){
+
+}
